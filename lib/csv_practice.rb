f8c600b29875d1aa8d4b38c873082235495576c3
Danielle Metzner <danimetzner@gmail.com>
2018-08-16 11:52:11 -0700

    Converting CSV to array of hashes - errors still

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -1,34 +1,40 @@
 # csv_practice.rb
 require 'csv'
 require 'awesome_print'
+require 'pry'
 
 # Part 1 - CSV Practice
 def load_data(filename)
-
+  data = CSV.open(filename, 'r', headers: true)
+  return data
 end
 
-def total_medals_per_country(olympic_data)
+ya = load_data('../data/athlete_events.csv')
+binding.pry
 
-end
 
-def save_medal_totals(filename, medal_totals)
-
-end
-
-# Part 2 - More Enumerable Practice
-
-def all_gold_medal_winners(olympic_data)
-
-end
-
-def metals_sorted_by_country(metal_totals)
-
-end
-
-def country_with_most_metals(metal_totals)
-
-end
-
-def athlete_height_in_inches(olympic_data)
-
-end
+# def total_medals_per_country(olympic_data)
+#
+# end
+#
+# def save_medal_totals(filename, medal_totals)
+#
+# end
+#
+# # Part 2 - More Enumerable Practice
+#
+# def all_gold_medal_winners(olympic_data)
+#
+# end
+#
+# def metals_sorted_by_country(metal_totals)
+#
+# end
+#
+# def country_with_most_metals(metal_totals)
+#
+# end
+#
+# def athlete_height_in_inches(olympic_data)
+#
+# end
